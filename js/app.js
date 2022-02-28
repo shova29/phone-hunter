ea88b70ffa31e35e35eca23dd8478138cb505882
shova29 <halima2996@gmail.com>
2022-03-01 00:24:04 +0600

    error part added

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@ const searchPhone = () => {
   const serachField = document.getElementById("search-field");
   const searchText = serachField.value;
   serachField.value = "";
-
+  document.getElementById("error-message").style.display = "none";
   if (searchText == "") {
-    console.log(searchText);
+    //   console.log(searchText);
+    const noplayerFound = document.getElementById("no-phone-found");
+    noplayerFound.innerText = `Please write something to display`;
+    return true;
   } else {
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}
-  `;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     try {
-      //   const response = fetch(url);
-      //   const data = response.json();
       fetch(url)
         .then((response) => response.json())
         .then((data) => console.log(data.data));
