780c152a93a58d145d2fe5288fcd6d3a03d13ce5
shova29 <halima2996@gmail.com>
2022-03-01 18:27:23 +0600

    spinner added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 const searchPhone = () => {
+  spinner("block");
   const serachField = document.getElementById("search-field");
   const searchText = serachField.value;
   serachField.value = "";
   document.getElementById("error-message").style.display = "none";
-  if (searchText == "") {
-    //   console.log(searchText);
-    const noPhoneFound = document.getElementById("no-phone-found");
-    noPhoneFound.innerText = `Please write something to display`;
+  const noPhoneFound = document.getElementById("no-phone-found");
+  // let timeout;
+  if (searchText === "") {
+    // console.log(searchText);
+    // spinner("block");
+    setTimeout(() => {
+      spinner("none");
+      noPhoneFound.innerText = `Please write something to display`;
+      noPhoneFound.style.display = "block";
+    }, 2000);
+    document.getElementById("phone-container").innerHTML = "";
     return true;
   } else {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
@@ -19,51 +27,87 @@ const searchPhone = () => {
         // .then((data) => console.log(data.data));
         .then((data) => displaySearchPhoneResult(data.data));
     } catch (error) {
+      // displayErrorMessage(errorMessage);
       // console.log(error);
       const errorMessage = document.getElementById("error-message");
       errorMessage.innerText = `No phone results have been found!`;
+      document.getElementById("phone-container").innerHTML = "";
+      return true;
+      // const myTimeout = setTimeout(displayError, 5000);
+      // clearTimeout(myTimeout);
     }
   }
+  // spinner("none");
 };
 
+/* const displayErrorMessage = (errorMessage) => {
+  // toggleSpinner("none");
+  // toggleSearchResult("none");
+  const errorMessage = document.getElementById("error-message");
+  errorMessage.innerText = `No phone results have been found!`;
+  return true;
+}; */
+/* const displaynNoPhoneFound = (noPhoneFound) => {
+  // toggleSpinner("none");
+  // toggleSearchResult("none");
+  const noPhoneFound = document.getElementById("no-phone-found");
+  noPhoneFound.innerText = `Please write something to display`;
+  return true;
+}; */
 //Display Phone Info Card
 const displaySearchPhoneResult = (phones) => {
+  spinner("block");
   const searchResult = document.getElementById("search-result");
   searchResult.textContent = "";
   document.getElementById("no-phone-found").style.display = "none";
+  document.getElementById("phone-details").style.display = "none";
   const errorMessage = document.getElementById("error-message");
   //checkif phone is found
-  if (
-    phones === null ||
-    phones === undefined ||
-    phones.length === 0 ||
-    phones === "number"
-  ) {
+  if (phones === null || phones === undefined || phones.length === 0) {
     // alert("No phone found");
-    document.getElementById("error-message").style.display = "block";
-    errorMessage.innerText = `No phone results have been found!`;
-    console.log(errorMessage);
+    spinner("block");
+    setTimeout(() => {
+      spinner("none");
+      errorMessage.innerText = `No phone results have been found!`;
+      errorMessage.style.display = "block";
+    }, 2000);
+    // const timeout = setTimeout(errorMessage, 10000);
+    // document.getElementById("error-message").style.display = "block";
+    // errorMessage.innerText = `No phone results have been found!`;
+    // // console.log(errorMessage);
+    // clearTimeout(timeout);
+    document.getElementById("phone-container").innerHTML = "";
     return true;
   }
   // console.log(phones.slice(0, 20));
 
   //phone found
-  phones.slice(0, 20).forEach((phone) => {
-    // console.log(phone);
-    const div = document.createElement("div");
-    div.classList.add("col");
-    div.style.fontSize = "16px";
-    div.innerHTML = `
-       <div class="card h-100 shadow rounded-3">
-          <img src="${phone.image}" class="card-img-top mt-3 p-3 w-50 mx-auto" alt="${phone.phone_name}">
-          <div class="card-body">
-              <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>
-              <p class="card-text"><span class="fw-bold"> Brand Name: </span>${phone.brand}</p>
-              <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-lg rounded btn-info text-white fw-bold">Details</button>
-          </div>
-       </div>`;
-    searchResult.appendChild(div);
-  });
+  if (phones.length > 0) {
+    setTimeout(() => {
+      spinner("none");
+      phones?.slice(0, 20).forEach((phone) => {
+        // console.log(phone);
+        const div = document.createElement("div");
+        div.classList.add("col");
+        div.style.fontSize = "16px";
+        div.innerHTML = `
+         <div class="card h-100 shadow rounded-3">
+            <img src="${phone.image}" class="card-img-top mt-3 p-3 w-50 mx-auto" alt="${phone.phone_name}">
+            <div class="card-body">
+                <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>
+                <p class="card-text"><span class="fw-bold"> Brand Name: </span>${phone.brand}</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-lg rounded btn-info text-white fw-bold">Details</button>
+            </div>
+         </div>`;
+        searchResult.appendChild(div);
+      });
+    }, 2000);
+    return true;
+  }
+
+  spinner("none");
+  // toggleSpinner("none");
+  // toggleSearchResult("block");
 };
 
 //Phone Details Load
@@ -77,6 +121,8 @@ const loadPhoneDetails = (id) => {
 //Display Phone Details Card
 const displayPhoneDetails = (phoneDetails) => {
   const phoneDetailsDiv = document.getElementById("phone-details");
+  document.getElementById("phone-details").style.display = "block";
+  // phoneDetailsDiv.textContent = "";
   phoneDetailsDiv.innerHTML = `
   <div class="card h-100 shadow rounded-3">
       <img src="${phoneDetails.image}" class="card-img-top mt-3 p-3 w-50 mx-auto" alt="${phoneDetails.name}">
@@ -98,4 +144,12 @@ const displayPhoneDetails = (phoneDetails) => {
           <p class="card-text"><span class="fw-bold"> Radio: </span>${phoneDetails.others.Radio}</p>
           <p class="card-text"><span class="fw-bold"> USB: </span>${phoneDetails.others.USB}</p>
           </div>`;
+  /* if (phoneDetails === 1) {
+    document.getElementById("phone-details").innerHTML = "";
+    return true;
+  } */
+};
+
+const spinner = (displayStyle) => {
+  document.getElementById("spinner").style.display = displayStyle;
 };
